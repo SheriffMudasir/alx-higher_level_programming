fed7292292b5a2048a2247b18373d82b26079bea
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-04 00:25:12 +0100

    This is the 0x01-python-if_else_loops_functions of the alx-higher_level_programming project

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-1000, 1000)
+num = abs(number) % 10
+if number < 0:
+    num = -num
+print("Last digit of {} is {}".format(number, num), end="")
+
+if num > 5:
+    print("greater than 5")
+elif num == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,3 @@
+GitHub repository: alx-higher_level_programming
+Directory: 0x01-python-if_else_loops_functions
+File: 0-positive_or_negative.py
