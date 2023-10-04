fbf1c65e436e163e5e57e7336509e9f755583e53
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-03 17:18:12 -0700

    Adding Finishing touches

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if number < 0:
 print("Last digit of {} is {}".format(number, num), end="")
 
 if num > 5:
-    print("greater than 5")
+    print(" and is greater than 5")
 elif num == 0:
-    print("0")
+    print(" and is 0")
 else:
-    print("less than 6 and not 0")
+    print(" and is less than 6 and not 0")
