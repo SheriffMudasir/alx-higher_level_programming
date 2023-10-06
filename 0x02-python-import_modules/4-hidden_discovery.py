0cd78b04595c191ea6ec4413819cefa4cec7e654
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-05 18:42:31 -0700

    Testing

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
- if __name__ == "__main__":
-     attributes = dir(hidden_4)
-     for i in attributes:
-         if i[:2] != "__":
-             print(attribute)
+if __name__ == "__main__":
+    import hidden_4
+
+
+    attributes = dir(hidden_4)
+    for i in attributes:
+        if i[:2] != "__":
+            print(attributes)
