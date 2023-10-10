7f43a154614d5360c2dd7859c2ac2551db0f03e5
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-10 02:47:21 +0100

    Solved the technical interview question

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * reverseList - This function reverse the list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: the reversed list
+ */
+void reverseList(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - This function checks if a list is a palindrone
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL)
+		return 1;
+
+	listint_t *current;
+	listint_t *reversedHead;
+
+	current = *head;
+	reversedHead = NULL;
+
+	reverseList(&current);
+	reversedHead = current;
+
+	while (*head != NULL && reversedHead != NULL)
+	{
+		if ((*head)->n != reversedHead->n)
+		{
+			return 0;
+		}
+		*head = (*head)->next;
+		reversedHead = reversedHead->next;
+	}
+	return 1;
+}
