80958ecfe054d44d027f61bf0ae0ea8a4491fcbb
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-26 03:49:12 +0100

    Tried fixing bugs

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 class Square:
-        """Define a class Square"""
+    """Define a class Square"""
 
-        def __init__(self, size=0):
-            """
-            Initialize a new square.
+    def __init__(self, size=0):
+        """
+        Initialize a new square.
 
-            Args:
-            size (int): The size of the new square.
-            """
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+        Args:
+        size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-        def area(self):
-            """
-            Calculate and return the area of the square.
+    def area(self):
+        """
+        Calculate and return the area of the square.
 
-            Returns:
-            int: The area of the square.
-            """
-            return self.__size * self.__size
+        Returns:
+        int: The area of the square.
+        """
+        return self.__size * self.__size
