89b15cf4eec6c392fc00ebcf64d7d2c63f779807
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-26 03:14:27 +0100

    D

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
 """Define a class Square"""
 
 class Square:
-    """This class represents a square"""
+        """This class represents a square"""
 
-    def __init__(self, size):
-        """
-        Initialize a new square.
+        def __init__(self, size):
+            """
+            Initialize a new square.
 
-        Args:
-        size (int): The size of the new square.
-        """
-        self.__size = size
+            Args:
+            size (int): The size of the new square.
+            """
+            self.__size = size
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
 class Square:
-    """Define a class Square"""
+        """Define a class Square"""
 
-    def __init__(self, size=0):
-        """
-        Initialize a new square.
+        def __init__(self, size=0):
+            """
+            Initialize a new square.
 
-        Args:
-        size (int): The size of the new square (default is 0).
-        """
-        self.size = size
+            Args:
+            size (int): The size of the new square (default is 0).
+            """
+            self.size = size
 
-    @property
-    def size(self):
-        """Retrieve the size of the square."""
-        return self.__size
+        @property
+        def size(self):
+            """Retrieve the size of the square."""
+            return self.__size
 
-    @size.setter
-    def size(self, value):
-        """Set the size of the square with type and value validation."""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+        @size.setter
+        def size(self, value):
+            """Set the size of the square with type and value validation."""
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
 
-    def area(self):
-        """
-        Calculate and return the area of the square.
+        def area(self):
+            """
+            Calculate and return the area of the square.
 
-        Returns:
-        int: The area of the square.
-        """
-        return self.__size * self.__size
+            Returns:
+            int: The area of the square.
+            """
+            return self.__size * self.__size
