fdce5f4de9dc564dcabc11d8806c9ad7509b92eb
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-30 04:51:09 +0100

    Addressing some errors

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -5,9 +5,7 @@ my_square_1 = Square(3)
 print(type(my_square_1))
 print(my_square_1.__dict__)
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+
 
 try:
     print(my_square_1.size)
@@ -18,6 +16,7 @@ try:
     print(my_square_1.__size)
 except Exception as e:
     print(e)
+
 
 try:
     my_square_3 = Square("3")
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""Define a class Square"""
 class Square:
-    """Define a class Square"""
+    """This class represent a Square"""
 
     def __init__(self, size=0):
         """
         Initialize a new square.
 
         Args:
-        size (int): The size of the new square.
+            size (int): The size of the new square. It should be a non-negative integer.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
