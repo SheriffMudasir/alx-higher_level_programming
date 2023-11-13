a8af22dfa66effcd8e9adc7d6b8469efa08e4f9b
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-11-13 15:15:56 -0800

    A

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,6 @@
 
 from models.base import Base
 
-
 class Rectangle(Base):
     """This Rectangle class inherits from Base class"""
 
@@ -16,48 +15,49 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        if not isinstance(self.__width, int):
-            raise TypeError(f"{self.__width} must be an integer")
-        if self.__width <= 0:
-            raise ValueError(f"{self.__width} must be > 0")
         return self.__width
 
     @property
     def height(self):
-        if not isinstance(self.__height, int):
-            raise TypeError(f"{self.__height} must be an integer")
-        if self.__height <= 0:
-            raise ValueError(f"{self.__height} must be > 0")
         return self.__height
 
     @property
     def x(self):
-        if not isinstance(self.__x, int):
-            raise TypeError(f"{self.__x} must be an integer")
-        if self.__x < 0:
-            raise ValueError(f"{self.__x} must be >= 0")
         return self.__x
 
     @property
     def y(self):
-        if not isinstance(self.__y, int):
-            raise TypeError(f"{self.__y} must be an integer")
-        if self.__y < 0:
-            raise ValueError(f"{self.__y} must be >= 0")
         return self.__y
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{self.__width} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{self.__width} must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{value} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{value} must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{value} must be an integer")
+        if value < 0:
+            raise ValueError(f"{value} must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{value} must be an integer")
+        if value < 0:
+            raise ValueError(f"{value} must be >= 0")
         self.__y = value
+
