64336fa7d30bdf9c0ef021a1a720424cdbdedd16
SheriffMudasir <sheriffmudasir2021@gmail.com>
2024-01-12 19:10:04 -0800

    Validating

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """
 This script list all states from the database hbtn_0e_0_usa
 """
@@ -12,6 +12,7 @@ def list_states(username, password, db_name):
     """
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
@@ -21,9 +22,10 @@ def list_states(username, password, db_name):
     cur.close()
     conn.close()
 
-    if __name__ == "__main__":
-        if len(sys.argv) != 4:
-            sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
     list_states(username, password, db_name)
+
